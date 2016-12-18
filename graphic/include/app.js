80d6542a07259d6215efb0fda58ac2e5aed33cf7
Claudio Cardinale <cardinale.claudio@gmail.com>
2016-12-18 23:40:46 +0100

    [graphic] starting drawing of pendulum

---
+++ graphic/include/app.js
@@ -0,0 +1,26 @@
+/**
+ * Created by claudio on 18/12/16.
+ */
+var DIM = 10;
+var INITIAL_POS =100;
+function drawPendulum(x, y) {
+    "use strict";
+    var myCanvas = document.getElementById("myCanvas");
+    var ctx = myCanvas.getContext("2d");
+    ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
+    ctx.lineWidth = 1;
+    ctx.fillStyle = "red";
+    ctx.strokeStyle = 'black';
+    ctx.moveTo(INITIAL_POS*DIM,INITIAL_POS*DIM);
+    ctx.lineTo(x*DIM,y*DIM);
+    ctx.stroke();
+    ctx.fillRect(INITIAL_POS*DIM,INITIAL_POS*DIM,DIM*4, DIM*4);
+    ctx.fillRect(x*DIM,y*DIM,DIM*4, DIM*4);
+}
+
+var i = 0;
+window.setInterval(function(){
+    "use strict";
+    drawPendulum(100+Math.cos(i/100*Math.PI)*50, 100-Math.sin(i/100*Math.PI)*50);
+    i++;
+},100);
